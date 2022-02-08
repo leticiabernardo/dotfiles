c0c837081188e0227a0faf3d8e730afcea23eae4
Letícia Bernardo <leticia.bernardo@mercadolivre.com>
2022-02-08 03:32:34 -0300

    Standardize code comments

--- setup/git.sh
+++ setup/git.sh
@@ -1,4 +1,4 @@
-# set up git using git config
+# Set up git using git config
 echo "Type in your full name"
 read name
 
@@ -12,6 +12,6 @@ git config --global user.name "$name"
 git config --global user.email "$email"
 git config --global github.user "$user"
 
-# generate ssh key
+# Generate ssh key
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -N "" -q
 pbcopy < ~/.ssh/id_rsa.pub
