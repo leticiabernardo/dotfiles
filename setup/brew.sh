c094a117f4a7932c9be5397c5a296ece36cd32c9
Letícia Bernardo <leticia.bernardo@mercadolivre.com>
2022-02-08 01:50:50 -0300

    Update installation by brew

--- setup/brew.sh
+++ setup/brew.sh
@@ -2,7 +2,12 @@
 echo "Installing Homebrew..."
 if test ! $(which brew); then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+	brew tap homebrew/bundle
 fi
+
+sudo chmod -R 755 /usr/local
+sudo chown -R $(whoami):admin /usr/local
 
 # update homebrew and package
 brew update & brew upgrade
