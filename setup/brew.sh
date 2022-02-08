907a918d5af0da8d11153149f452f1f3a5fc8314
Leticia <leticia.bernardo@mercadolivre.com>
2022-02-07 22:22:07 -0300

    Fix installation with brew cask

--- setup/brew.sh
+++ setup/brew.sh
@@ -1,42 +1,25 @@
 # install homebrew
+echo "Installing Homebrew..."
 if test ! $(which brew); then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 # update homebrew and package
-brew update & brew upgrade 
+brew update & brew upgrade
 
-# install all packages
-brew install git
-brew install gcc
-brew install python
-brew install go
-brew install node
-brew install npm
-brew install yarn
-brew install docker
-brew install docker-compose
-brew install kubernetes-cli
-brew install mysql
-brew install sqlite
-brew install mongodb
-brew install postgresql
-brew install opencv
-brew install tree
-brew install wget
+# install all commandlines
+echo "Checking and installing command lines with brew..."
+while read -r line; do
+	echo "Installing $line"
+	brew list --versions $line || brew install $line
+done < files/apps/commandlines.txt
 
-# install all apps
-brew tap caskroom/cask
-brew cask install google-chrome
-brew cask install visual-studio-code
-brew cask install iterm2
-brew cask install spotify
-brew cask install discord
-brew cask install mysqlworkbench
-brew cask install postman
-brew cask install pycharm
-brew cask install slack
-brew cask install evernote
+# install all applications
+echo "Checking and installing applications with brew cask..."
+while read -r line; do
+	echo "Installing $line"
+	brew install --cask $line --quiet
+done < files/apps/applications.txt
 
 # remove outdated versions
 brew cleanup
