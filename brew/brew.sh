c10f805024fb28253dea780fa02b7e95a135827f
Letícia Bernardo <13439423+leticiabernardo@users.noreply.github.com>
2022-02-19 19:36:03 -0300

    Configure the Brewfile and bundle to install the apps

---
+++ brew/brew.sh
@@ -0,0 +1,18 @@
+# Install homebrew & bundle
+echo "Installing Homebrew..."
+if test ! $(which brew); then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+	brew tap homebrew/bundle
+fi
+
+sudo chmod -R 755 /usr/local
+sudo chown -R $(whoami):admin /usr/local
+
+# Update homebrew and package
+brew update & brew upgrade
+
+brew bundle
+
+# Remove outdated versions
+brew cleanup
